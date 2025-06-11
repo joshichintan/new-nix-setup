15b3c2ce4ffce304891fdf978bebd2d4ca3cb201
chintan <chintanjoshi2012@gamil.com>
2025-06-10 17:41:03 -0700

    simple test to get nix working

--- flake.nix
+++ flake.nix
@@ -5,41 +5,32 @@
     nixpkgs.url        = "github:NixOS/nixpkgs/nixos-unstable";
     darwin.url         = "github:LnL7/nix-darwin";
     home-manager.url   = "github:nix-community/home-manager";
-    flake-utils.url    = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, darwin, home-manager, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        # nix-darwin host configurations
-        darwinConfigurations = {
-          personal = darwin.lib.darwinSystem {
-            system                 = system;
-            modules                = [ ./hosts/darwin/personal.nix ];
-            pkgs                   = pkgs;
-            darwinUseHomeManager   = true;
-            homeManagerConfiguration = ./users/chintan.nix;
-          };
+  outputs = { self, nixpkgs, darwin, home-manager, ... }:
+  let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    # nix-darwin host configurations
+    darwinConfigurations = {
+      personal = darwin.lib.darwinSystem {
+        services.nix-daemon.enable = true;
+        nix.settings.experimental-features = [ "nix-command" "flakes" ];
+        programs.zsh.enable = true;
+        system.configurationRevision = self.rev or "dirty" or null;
+        system.stateVersion = 5;
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+    };
 
-          work = darwin.lib.darwinSystem {
-            system                 = system;
-            modules                = [ ./hosts/darwin/work.nix ];
-            pkgs                   = pkgs;
-            darwinUseHomeManager   = true;
-            homeManagerConfiguration = ./users/chintan.nix;
-          };
-        };
-
-        # standalone home-manager configuration
-        homeConfigurations = {
-          chintan = home-manager.lib.homeManagerConfiguration {
-            pkgs          = pkgs;
-            modules       = [ ./users/chintan.nix ];
-            homeDirectory = "/Users/chintan";
-            username      = "chintan";
-          };
-        };
-      });
+    # standalone home-manager configuration
+    homeConfigurations = {
+      chintan = home-manager.lib.homeManagerConfiguration {
+        pkgs          = pkgs;
+        modules       = [ ./users/chintan.nix ];
+        homeDirectory = "/Users/chintan";
+        username      = "chintan";
+      };
+    };
+  };
 } 
