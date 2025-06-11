4c8dc4f994296ed4f6c86eb6850c1d7efec0c086
chintan <chintanjoshi2012@gamil.com>
2025-06-10 17:50:25 -0700

    fix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         services.nix-daemon.enable = true;
         nix.settings.experimental-features = [ "nix-command" "flakes" ];
         programs.zsh.enable = true;
-        system.configurationRevision = self.rev or "dirty" or null;
+        system.configurationRevision = self.rev or self.dirtyRev or null;
         system.stateVersion = 5;
         nixpkgs.hostPlatform = "aarch64-darwin";
       };
