11e0a00838d15c5793e047777bf16a348f79698c
chintan <chintanjoshi2012@gamil.com>
2025-06-27 18:44:54 -0700

    aliases are back

--- home/nix-darwin.nix
+++ home/nix-darwin.nix
@@ -231,60 +231,28 @@
 
   programs.zoxide.enable = true;
 
-  # Nix command functions for easier usage
-  programs.zsh.initExtra = ''
-    # Home Manager functions
-    hm() {
-      nix run .#homeConfigurations.$(whoami)@$(hostname | cut -d'.' -f1).activationPackage
-    }
-    
-    hm-build() {
-      nix build .#homeConfigurations.$(whoami)@$(hostname | cut -d'.' -f1).activationPackage
-    }
-    
-    hm-check() {
-      nix build .#homeConfigurations.$(whoami)@$(hostname | cut -d'.' -f1).activationPackage --dry-run
-    }
-    
-    # nix-darwin functions
-    darwin() {
-      nix run .#darwinConfigurations.$(hostname | cut -d'.' -f1).system
-    }
-    
-    darwin-build() {
-      nix build .#darwinConfigurations.$(hostname | cut -d'.' -f1).system
-    }
-    
-    darwin-check() {
-      nix build .#darwinConfigurations.$(hostname | cut -d'.' -f1).system --dry-run
-    }
-    
-    # General Nix functions
-    nix-update() {
-      nix flake update
-    }
-    
-    nix-gc() {
-      nix-store --gc
-    }
-    
-    nix-clean() {
-      nix-collect-garbage -d
-    }
-    
-    # Quick rebuild functions
-    rebuild() {
-      darwin && hm
-    }
-    
-    rebuild-home() {
-      hm
-    }
-    
-    rebuild-system() {
-      darwin
-    }
-  '';
+  # Nix command aliases for easier usage
+  programs.zsh.shellAliases = {
+    # Home Manager aliases (dynamic user/hostname detection)
+    hm = "nix run .#homeConfigurations.$(whoami)@$(hostname | cut -d'.' -f1).activationPackage";
+    hm-build = "nix build .#homeConfigurations.$(whoami)@$(hostname | cut -d'.' -f1).activationPackage";
+    hm-check = "nix build .#homeConfigurations.$(whoami)@$(hostname | cut -d'.' -f1).activationPackage --dry-run";
+
+    # nix-darwin aliases (dynamic hostname detection)
+    darwin = "nix run .#darwinConfigurations.$(hostname | cut -d'.' -f1).system";
+    darwin-build = "nix build .#darwinConfigurations.$(hostname | cut -d'.' -f1).system";
+    darwin-check = "nix build .#darwinConfigurations.$(hostname | cut -d'.' -f1).system --dry-run";
+
+    # General Nix aliases
+    nix-update = "nix flake update";
+    nix-gc = "nix-store --gc";
+    nix-clean = "nix-collect-garbage -d";
+
+    # Quick rebuild aliases
+    rebuild = "darwin && hm"; # Rebuild both system and home
+    rebuild-home = "hm"; # Rebuild only home
+    rebuild-system = "darwin"; # Rebuild only system
+  };
 
   programs.vscode = {
     enable = true;
