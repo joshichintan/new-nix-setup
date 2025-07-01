85318408c0ad63012fd3109b67cfded8deedefdb
chintan <chintanjoshi2012@gamil.com>
2025-06-30 21:34:37 -0700

    snacks.nvim

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -169,6 +169,41 @@
         enable = true;
       };
 
+          utility = {
+      snacks-nvim = {
+        enable = true;
+        setupOpts = {
+          picker = {enabled = true;};
+          explorer = {enabled = true;};
+          bigfile = {enabled = true;};
+          debug = {enabled = true;};
+          indent = {enabled = true;};
+          image = {enabled = true;};
+          notifier = {enabled = true;};
+          scratch = {enabled = true;};
+          statuscolumn = {enabled = false;};
+          quickfile = {enabled = true;};
+          zen = {enabled = true;};
+          # dashboard = {enabled = true;};
+        };
+      };
+      vim-wakatime.enable = true;
+      # Use snacks image so this is no needed.
+      # images = {
+      #   image-nvim = {
+      #     enable = true;
+      #     setupOpts = {
+      #       integrations = {
+      #         markdown.enable = true;
+      #       };
+      #     };
+      #   };
+      # };
+      preview.markdownPreview = {
+        enable = true;
+      };
+    };
+
       # Custom Lua configuration (only for terminal detection)
       luaConfigRC.themeSetup = ''
         -- Terminal-specific colorscheme and termguicolors
