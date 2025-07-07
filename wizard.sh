a0144c0bc2cd87372d22a2c1eee045aed2ec90d6
chintan <chintanjoshi2012@gamil.com>
2025-07-06 21:19:46 -0700

    Revert "fix"
    This reverts commit 95c34a7765adca3ad6ac47d504882fe467fd5df2.

--- wizard.sh
+++ wizard.sh
@@ -664,7 +664,7 @@ ask_installation_preference() {
     echo "  2) Interactive mode (ask at each step)"
     echo
     read -p "Enter your choice (1-2): " -n 1 -r
-    clear_prompt_block 5
+    clear_prompt_block 6
     
     case $REPLY in
         1)
