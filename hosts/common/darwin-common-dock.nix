71221c5f7d138799f386d0055ed2a9b1a5ac7f9e
Chintan Joshi <chintanjoshi2012@gmail.com>
2025-09-25 20:50:32 +0000

    remove random default dock items

--- hosts/common/darwin-common-dock.nix
+++ hosts/common/darwin-common-dock.nix
@@ -2,9 +2,6 @@
   system.defaults.dock = {
     persistent-apps = [
       "/Applications/Google Chrome.app"
-      "/Applications/Firefox.app"
-      "/Applications/Visual Studio Code.app"
-      "/Applications/Spotify.app"
     ];
   };
 }
