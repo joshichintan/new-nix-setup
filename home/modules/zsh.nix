cd180bf45f7593b4e1709ea9ec5cfd8ec1c1c462
Chintan Joshi <chintanjoshi2012@gmail.com>
2025-09-25 00:32:51 -0700

    updated aliases

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -57,25 +57,67 @@
     };
 
     shellAliases = {
-      # Home Manager aliases (dynamic user/hostname detection)
-      hm = "nix run \${NIX_USER_CONFIG_PATH:-.}#homeConfigurations.\$(whoami)@\$(hostname | cut -d'.' -f1).activationPackage";
-      hm-build = "nix build \${NIX_USER_CONFIG_PATH:-.}#homeConfigurations.\$(whoami)@\$(hostname | cut -d'.' -f1).activationPackage";
-      hm-check = "nix build \${NIX_USER_CONFIG_PATH:-.}#homeConfigurations.\$(whoami)@\$(hostname | cut -d'.' -f1).activationPackage --dry-run";
-
-      # nix-darwin aliases (dynamic hostname detection)
-      darwin = "sudo nix run nix-darwin#darwin-rebuild -- switch --flake \${NIX_USER_CONFIG_PATH:-.}#\$(hostname | cut -d'.' -f1)";
-      darwin-build = "nix build \${NIX_USER_CONFIG_PATH:-.}#darwinConfigurations.\$(hostname | cut -d'.' -f1).system";
-      darwin-check = "nix build \${NIX_USER_CONFIG_PATH:-.}#darwinConfigurations.\$(hostname | cut -d'.' -f1).system --dry-run";
-
       # General Nix aliases
-      nix-update = "nix flake update --flake \${NIX_USER_CONFIG_PATH:-.}";
+      nix-update = "nix flake update --flake .";
       nix-gc = "nix-store --gc";
       nix-clean = "nix-collect-garbage -d";
-
-      # Quick rebuild aliases
-      rebuild = "darwin && hm"; # Rebuild both system and home
-      rebuild-home = "hm"; # Rebuild only home
-      rebuild-system = "darwin"; # Rebuild only system
     };
+
+    # Functions for dynamic user/hostname detection
+    initExtra = ''
+      # Home Manager functions
+      hm() {
+        local user="$(whoami)"
+        local host="$(hostname | cut -d'.' -f1)"
+        local flake_ref=".#homeConfigurations.\"$user@$host\".activationPackage"
+        nix run "$flake_ref"
+      }
+      
+      hm-build() {
+        local user="$(whoami)"
+        local host="$(hostname | cut -d'.' -f1)"
+        local flake_ref=".#homeConfigurations.\"$user@$host\".activationPackage"
+        nix build "$flake_ref"
+      }
+      
+      hm-check() {
+        local user="$(whoami)"
+        local host="$(hostname | cut -d'.' -f1)"
+        local flake_ref=".#homeConfigurations.\"$user@$host\".activationPackage"
+        nix build "$flake_ref" --dry-run
+      }
+
+      # nix-darwin functions
+      darwin() {
+        local host="$(hostname | cut -d'.' -f1)"
+        local flake_ref=".#$host"
+        sudo nix run nix-darwin#darwin-rebuild -- switch --flake "$flake_ref"
+      }
+      
+      darwin-build() {
+        local host="$(hostname | cut -d'.' -f1)"
+        local flake_ref=".#darwinConfigurations.$host.system"
+        nix build "$flake_ref"
+      }
+      
+      darwin-check() {
+        local host="$(hostname | cut -d'.' -f1)"
+        local flake_ref=".#darwinConfigurations.$host.system"
+        nix build "$flake_ref" --dry-run
+      }
+
+      # Quick rebuild functions
+      rebuild() {
+        darwin && hm
+      }
+      
+      rebuild-home() {
+        hm
+      }
+      
+      rebuild-system() {
+        darwin
+      }
+    '';
   };
 }
