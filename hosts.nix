bc7327266529314308a5f122f26e3dd87d81dc4d
Chintan Joshi <chintan.joshi@Chintans-Virtual-Machine.local>
2025-10-22 22:21:09 -0700

    ignore hosts.nix

--- hosts.nix
+++ hosts.nix
@@ -1,10 +1,3 @@
 # Host configurations
 {
-  chintan = {
-    hostname = "chintan";
-    username = "nix-darwin";
-  };
-  Chintans-Virtual-Machine = {
-    hostname = "Chintans-Virtual-Machine";
-    username = "chintan.joshi";
-  };}
+}
